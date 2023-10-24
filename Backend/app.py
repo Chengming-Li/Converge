d5a2dad6c879b067ff2701d5bbc44a0e63de7af8
Chengming Li <chengming.li@berkeley.edu>
2023-10-24 01:12:09 -0700

    fixed dockerfile and added basic flask functionalities to app.py

--- Backend/app.py
+++ Backend/app.py
@@ -1,7 +1,19 @@
+import os
+import psycopg2
+from dotenv import load_dotenv
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return "Hello World!"
+# loads environment variables from .env and gets the database URL
+load_dotenv()
+url = os.getenv("DATABASE_URL")
+
+connection = psycopg2.connect(url)  # establishes connection to database
+
+@app.get('/api/')  # exposes endpoint at "/api/"
+def defaultApiCall():
+    return "Hello World"
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=True)
