b15e54d146af95582acb43b1763138a1262d2647
Chengming-Li <115936688+Chengming-Li@users.noreply.github.com>
2023-10-26 00:30:44 -0700

    Set up backend
    * fix dockerfile and add basic flask functionalities
    
    * implement endpoints
    
    * clean up directory and add documentation
    
    * implement more endpoints
    
    * fix bugs
    
    * fix more bugs and added more documentation

---
+++ Backend/src/app.py
@@ -0,0 +1,52 @@
+import os
+from flask import Flask
+import psycopg2
+from dotenv import load_dotenv
+from actions import createUser, getUser, getTable, deleteTable, deleteUser, startInterval, endInterval, editInterval, deleteInterval
+
+app = Flask(__name__)
+
+# exposes endpoints
+@app.get('/api')
+def defaultApiCall():
+    return "Hello World"
+
+@app.post('/api/user')
+def create_user_profile():
+    return createUser()
+
+@app.get('/api/user/<int:user_id>')
+def get_user_profile(user_id):
+    return getUser(user_id)
+   
+@app.get('/api/table/<string:tableName>')
+def fetch_table(tableName):
+    return getTable(tableName)
+
+@app.delete('/api/user/<int:user_id>')
+def delete_user_profile(user_id):
+    return deleteUser(user_id)
+
+@app.post('/api/interval')
+def start_interval():
+    return startInterval()
+
+@app.put('/api/interval/end/<int:interval_id>')
+def end_interval(interval_id):
+    return endInterval(interval_id)
+
+@app.put('/api/interval/<int:interval_id>')
+def edit_interval(interval_id):
+    return editInterval(interval_id)
+
+@app.delete('/api/interval/<int:interval_id>')
+def delete_interval(interval_id):
+    return deleteInterval(interval_id)
+
+# THIS IS ONLY FOR DEVELOPMENT, REMOVE WHEN DEPLOYING
+@app.delete('/api/delete/table/<string:tableName>')
+def delete_table(tableName):
+    return deleteTable(tableName)
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000, debug=True)
