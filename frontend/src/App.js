fae6de752048e01d85b6511bbd043fded98fab7d
Chengming Li <chengming.li@berkeley.edu>
2023-11-04 16:39:58 -0700

    fetch user data from frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
+import Input from './Input';
 
-const API = "http://localhost:5000/api/table/users"
+const API = "http://localhost:5000/api/user/913900508510060545"
 function App() {
   const [data, setData] = useState(null);
   const [error, setError] = useState(null);
@@ -14,7 +15,7 @@ function App() {
       }
       return response.json(); // Assuming your server returns JSON
     }).then((data) => {
-      setData(data[0].username);
+      setData(data);
       setLoading(false)
     }).catch((error) => {
       setError(error.message)
@@ -23,13 +24,42 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        {loading ? <p>Loading...</p> 
-        : error ? <p>{error}</p>
-        : <p>{data}</p>
+    <div className="background">
+      <Input />
+      <div className="App">
+        {loading ? <p style={{ marginTop: "20px"}}>Loading...</p>
+          : error ? <p style={{ color: "red", marginTop: "20px" }}>{error}</p>
+          : <>
+            <p style={{ marginTop: "20px", fontWeight: 'bold' }}>User Info</p>
+            <p>Email: {data.userInfo.email}</p>
+            <p>Username: {data.userInfo.username}</p>
+            <p>User ID: {data.userInfo.id}</p>
+            <p>Timezone: {data.userInfo.timezone}</p>
+
+            <p style={{ marginTop: "20px", fontWeight: 'bold' }}>Active Interval</p>
+            <p>Name: {data.activeInterval.name}</p>
+            <p>Project ID: {data.activeInterval.project_id}</p>
+            <p>Interval ID: {data.activeInterval.interval_id}</p>
+            <p>Start Time: {data.activeInterval.start_time}</p>
+            <p>End Time: {data.activeInterval.end_time}</p>
+
+            <p style={{ marginTop: "20px", fontWeight: 'bold' }}>Past Intervals</p>
+            <ul>
+              {data.intervals.map((item) => (
+                <li key={item.interval_id}>
+                  <>
+                    <p>Name: {item.name}</p>
+                    <p>Project ID: {item.project_id}</p>
+                    <p>Interval ID: {item.interval_id}</p>
+                    <p>Start Time: {item.start_time}</p>
+                    <p>End Time: {item.end_time}</p>
+                  </>
+                </li> 
+              ))}
+            </ul>
+          </>
         }
-      </header>
+      </div>
     </div>
   );
 }
---
+++ frontend/src/Input.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function Input() {
+  return (
+    <div className="Input"> 
+      <h1>User Information</h1>
+    </div>
+  );
+}
+
+export default Input;
