a5e336f6cb8307702e2f5884b6c8b9d6cf30dd2c
Chengming Li <chengming.li@berkeley.edu>
2023-11-04 00:33:46 -0700

    set up react foundation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,34 @@
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
+const API = "http://localhost:5000/api/table/users"
 function App() {
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch(API).then((response) => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json(); // Assuming your server returns JSON
+    }).then((data) => {
+      setData(data[0].username);
+      setLoading(false)
+    }).catch((error) => {
+      setError(error.message)
+      setLoading(false)
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Hello World
-        </p>
+        {loading ? <p>Loading...</p> 
+        : error ? <p>{error}</p>
+        : <p>{data}</p>
+        }
       </header>
     </div>
   );
