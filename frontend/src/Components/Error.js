e3f105e4af7125f56091f469e8ecf7d2fbaec91c
Chengming Li <chengming.li@berkeley.edu>
2023-12-24 18:51:02 -0800

    implemented new error display

---
+++ frontend/src/Components/Error.js
@@ -0,0 +1,29 @@
+import React, { useState }from 'react';
+import '../Styles/Components.css';
+
+function Error({messages, setMessages}) {
+
+    return (
+        <div style={{position: "absolute", width: "auto", height: "auto", right: "10px", bottom: "2px"}}>
+            {
+                messages.map((error, index) => [
+                    <div className='error' key={error}>
+                        <button onClick={() => {
+                            setMessages(messages.slice(0, index).concat(messages.slice(index + 1)));
+                        }}>X</button>
+                        <span>{error}</span>
+                    </div>
+                ])
+            }
+        </div>
+    )
+    /*
+    {
+                messages.messages.map((error, index) => [
+                    <p key={index}>{error}</p>
+                ])
+            }
+    */
+}
+
+export default Error;
