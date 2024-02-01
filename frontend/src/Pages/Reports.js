5fc7508af841f209c1e6c71d03159ab4aa01f044
Chengming Li <chengming.li@berkeley.edu>
2024-02-01 15:25:53 -0800

    add button element

--- frontend/src/Pages/Reports.js
+++ frontend/src/Pages/Reports.js
@@ -1,3 +1,4 @@
+import 'react-date-range/dist/styles.css';
 import '../Styles/Reports.css';
 import React, { useState, useEffect, useRef } from 'react';
 import Sidebar from '../Components/Sidebar';
@@ -6,7 +7,6 @@ import Error from '../Components/Error';
 import Loading from '../Components/Loading';
 
 import { DateRangePicker } from 'react-date-range';
-import 'react-date-range/dist/styles.css';
 
 const userID = "931452152733499393";
 const userDataAPI = "http://localhost:5000/api/user/"
@@ -16,13 +16,15 @@ const Reports = () => {
     const [collapsedMenu, setCollapsedMenu] = useState(false);
     const [errors, setErrors] = useState([]);
     const [userInfo, setUserInfo] = useState(null);
-	
-	const [selectionRange, setSelectionRange] = useState({
-		startDate: new Date(),
-		endDate: new Date(),
-		key: 'selection',
-	});
-    const [intervals, setIntervals] = useState([]);
+    const [dayMenu, setDayMenu] = useState(false);
+    const dayMenuRef = useRef(null);
+
+    const [selectionRange, setSelectionRange] = useState({
+        startDate: new Date(),
+        endDate: new Date(),
+        key: 'selection',
+    });
+  	const [intervals, setIntervals] = useState([]);
     const [projects, setProjects] = useState([]);
 
     const getIntervals = (startDate, endDate, project) => {
@@ -51,20 +53,22 @@ const Reports = () => {
             setErrors(oldErrors => [...oldErrors, error.message]);
             setLoading(false);
         });
+
+        const handleClickOutside = (event) => {
+            if (dayMenuRef.current && !dayMenuRef.current.contains(event.target)) {
+                setDayMenu(false);
+            }
+        }
+        document.addEventListener('mousedown', handleClickOutside);
+
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside);
+        };
     }, []);
 
-	const customStyles = {
-    	calendar: {
-      		color: 'red',
-    	},
-   		ranges: {
-      		background: 'blue',
-    	},
-	};
-
-	const handleSelect = (ranges) => {
-		setSelectionRange(ranges.selection);	
-	}
+    const handleSelect = (ranges) => {
+        setSelectionRange(ranges.selection);
+    }
 
     return (
         <div className='App'>
@@ -75,13 +79,27 @@ const Reports = () => {
             />
             <Header ToggleMenu={() => { setCollapsedMenu(!collapsedMenu) }} />
             <Sidebar collapsed={collapsedMenu} username={userInfo ? userInfo.username : "No User"} pfp={userInfo ? userInfo.profile_picture : null} />
-            <DateRangePicker
-                ranges={[selectionRange]}
-                onChange={handleSelect}
-				rangeColors={["purple"]}
-				className="dateRange"
-				styles={customStyles}
-            />
+            <button className="dayMenuButton" onClick={() => { setDayMenu(true) }} style={{ left: `calc(${collapsedMenu ? '55px' : '125px'} + 50%)` }}>
+				{"<   "} {selectionRange.startDate.toLocaleDateString('en-US', {
+  					year: 'numeric',
+  					month: '2-digit',
+					day: '2-digit'
+				})} - {selectionRange.endDate.toLocaleDateString('en-US', {
+					year: 'numeric',
+					month: '2-digit',
+					day: '2-digit'
+				})} {"   >"}
+			</button>
+            {dayMenu &&
+                <div ref={dayMenuRef} style={{ position: 'absolute', left: `calc(${collapsedMenu ? '55px' : '125px'} + 50%)`, transform: "translateX(-50%)", top: '120px', borderRadius: "10px", overflow: "hidden" }}>
+                    <DateRangePicker
+                        ranges={[selectionRange]}
+                        onChange={handleSelect}
+                        rangeColors={["darkslateblue"]}
+                        className="dateRange"
+                        fixedHeight={true}
+                    />
+                </div>}
         </div>
     );
 }
