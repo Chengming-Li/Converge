4dda50b46dccaf558d5dda50de1b4fe6d3237692
Chengming-Li <chengming.li@berkeley.edu>
2025-07-27 16:32:47 -0700

    add website

--- start_k8s.sh
+++ start_k8s.sh
@@ -1,7 +1,32 @@
-kubectl apply -f k8s/env-configmap.yaml
-kubectl apply -f k8s/backend-deployment.yaml
-kubectl apply -f k8s/backend-service.yaml
-kubectl apply -f k8s/frontend-deployment.yaml
-kubectl apply -f k8s/frontend-service.yaml
+#!/bin/bash
+SHUT_DOWN=0
+
+while [ $# -gt 0 ]; do
+  case "$1" in
+    --shutdown)
+      SHUT_DOWN=1
+      shift
+      ;;
+    *)
+      echo "Unknown flag: $1"
+      shift
+      ;;
+  esac
+done
+
+if [ "$SHUT_DOWN" -eq 1 ]; then
+    echo "Deleting deployments"
+    kubectl delete deployments frontend backend website
+    echo "Deleting services"
+    kubectl delete services frontend backend website
+else
+    kubectl apply -f k8s/env-configmap.yaml
+    kubectl apply -f k8s/backend-deployment.yaml
+    kubectl apply -f k8s/backend-service.yaml
+    kubectl apply -f k8s/frontend-deployment.yaml
+    kubectl apply -f k8s/frontend-service.yaml
+    kubectl apply -f k8s/website-deployment.yaml
+    kubectl apply -f k8s/website-service.yaml
+fi
 
 # kubectl rollout restart deployment frontend
