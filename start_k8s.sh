33085009ef81f2032692c4c8f9459bfb4f6c3ab1
Chengming-Li <chengming.li@berkeley.edu>
2025-07-26 17:08:25 -0700

    fix k8s

---
+++ start_k8s.sh
@@ -0,0 +1,7 @@
+kubectl apply -f k8s/env-configmap.yaml
+kubectl apply -f k8s/backend-deployment.yaml
+kubectl apply -f k8s/backend-service.yaml
+kubectl apply -f k8s/frontend-deployment.yaml
+kubectl apply -f k8s/frontend-service.yaml
+
+# kubectl rollout restart deployment frontend
